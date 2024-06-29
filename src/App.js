174ae7cb7331f1d7f992cddd30de72ea687f63f7
Bharti Jangir <142597742+jollyhub8278@users.noreply.github.com>
2024-06-29 21:59:30 +0530

    hii

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import Layout from "./components/layout/Layout";
 import About from "./pages/About/About";
+import Projects from "./pages/Projects/Projects";
+import Techstack from "./pages/Techstack/Techstack";
 
 function App() {
   return (
     <div >
-      <Layout/>
-      <About/>
+      <Layout />
+      <div className="container">
+        <About />
+        <Techstack/>
+        <Projects/>
+      </div>
     </div>
   );
 }
---
+++ src/utils/TechstackList.js
@@ -0,0 +1,81 @@
+import { FaHtml5 } from "react-icons/fa";
+import { DiCssTricks } from "react-icons/di";
+import { IoLogoJavascript } from "react-icons/io";
+import { FaReact } from "react-icons/fa";
+import { IoLogoNodejs } from "react-icons/io";
+import { SiExpress } from "react-icons/si";
+import { SiMongodb } from "react-icons/si";
+import { FaBootstrap } from "react-icons/fa";
+import { FaCode } from "react-icons/fa";
+import { RiMiniProgramFill } from "react-icons/ri";
+import { PiTreeStructureDuotone } from "react-icons/pi";
+import { FaFigma } from "react-icons/fa";
+import { FaGitAlt } from "react-icons/fa";
+
+export const TechstackList = [
+    {
+        _id: 1,
+        name: 'HTML',
+        icon: FaHtml5,
+    },
+    {
+        _id: 2,
+        name: 'CSS',
+        icon: DiCssTricks,
+    },
+    {
+        _id: 3,
+        name: 'Javascript',
+        icon: IoLogoJavascript,
+    },
+    {
+        _id: 4,
+        name: 'React',
+        icon: FaReact,
+    },
+    {
+        _id: 5,
+        name: 'NodeJs',
+        icon: IoLogoNodejs,
+    },
+    {
+        _id: 6,
+        name: 'ExpressJs',
+        icon: SiExpress ,
+    },
+    {
+        _id: 7,
+        name: 'MongoDb',
+        icon: SiMongodb ,
+    },
+    {
+        _id: 8,
+        name: 'C/C++',
+        icon: FaCode,
+    },
+    {
+        _id: 9,
+        name: 'DSA',
+        icon: PiTreeStructureDuotone,
+    },
+    {
+        _id: 10,
+        name: 'CP',
+        icon: RiMiniProgramFill,
+    },
+    {
+        _id: 11,
+        name: 'BootStrap',
+        icon: FaBootstrap,
+    },
+    {
+        _id: 12,
+        name: 'Figma',
+        icon: FaFigma,
+    },
+    {
+        _id: 13,
+        name: 'Git/Github',
+        icon: FaGitAlt,
+    }
+]
