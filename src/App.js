769af0e296e5238c90ab005e14dda613827536e6
Bharti Jangir <142597742+jollyhub8278@users.noreply.github.com>
2024-11-09 13:47:21 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
+import React from "react";
 import Layout from "./components/layout/Layout";
 import About from "./pages/About/About";
-import Contact from "./pages/Contact/Contact";
+// import Contact from "./pages/Contact/Contact";
 import Education from "./pages/Education/Education";
 import Projects from "./pages/Projects/Projects";
 import Techstack from "./pages/Techstack/Techstack";
 import WorkExp from "./pages/workExp/workExp";
+import ScrollToTop from "react-scroll-to-top";
+import ScrollProgressLine from "@yiyb0603/react-scroll-progress-line";
 
 function App() {
   return (
-    <div >
+    <>
       <Layout />
       <div className="container">
         <About />
-        <Education/>
-        <Techstack/>
-        <Projects/>
-        <WorkExp/>
-        <Contact/>
+        <Education />
+        <Techstack />
+        <Projects />
+        <WorkExp />
+        {/* <Contact /> */}
       </div>
-    </div>
+      <ScrollToTop
+        smooth
+        color="white"
+        style={{ backgroundColor: "blue", borderRadius: "80px" }}
+      />
+      <ScrollProgressLine
+        backgroundColor="gray"
+        progressColor="blue"
+        height="8px"
+      />
+    </>
   );
 }
 
