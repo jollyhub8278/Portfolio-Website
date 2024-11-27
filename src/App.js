6539f0a19c7c7cbad0815d0fa3d87fd23f0c25be
Bharti Jangir <142597742+jollyhub8278@users.noreply.github.com>
2024-11-27 16:13:42 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from "react";
 import Layout from "./components/layout/Layout";
 import About from "./pages/About/About";
-// import Contact from "./pages/Contact/Contact";
+import Contact from "./pages/Contact/Contact";
 import Education from "./pages/Education/Education";
 import Projects from "./pages/Projects/Projects";
 import Techstack from "./pages/Techstack/Techstack";
 import WorkExp from "./pages/workExp/workExp";
 import ScrollToTop from "react-scroll-to-top";
-import ScrollProgressLine from "@yiyb0603/react-scroll-progress-line";
+import { TfiArrowCircleUp } from "react-icons/tfi";
+import MobileNav from "./components/MobileNav/MobileNav";
+// import ScrollProgressLine from "@yiyb0603/react-scroll-progress-line";
 
 function App() {
   return (
     <>
+      <MobileNav/>
       <Layout />
       <div className="container">
         <About />
@@ -19,17 +22,13 @@ function App() {
         <Techstack />
         <Projects />
         <WorkExp />
-        {/* <Contact /> */}
+        <Contact />
       </div>
       <ScrollToTop
         smooth
         color="white"
-        style={{ backgroundColor: "blue", borderRadius: "80px" }}
-      />
-      <ScrollProgressLine
-        backgroundColor="gray"
-        progressColor="blue"
-        height="8px"
+        component={<TfiArrowCircleUp />}
+        style={{height:"50px",width:"50px", borderRadius:"25px"}}
       />
     </>
   );
--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import { toast } from "react-toastify";
-import axios from "axios";
+// import { toast } from "react-toastify";
+// import axios from "axios";
 import "./Contact.css";
-import Rotate from "react-reveal/Rotate";
-import LightSpeed from "react-reveal/LightSpeed";
+// import Rotate from "react-reveal/Rotate";
+// import LightSpeed from "react-reveal/LightSpeed";
 import { BsFacebook, BsGithub, BsLinkedin } from "react-icons/bs";
 const Contact = () => {
   const [name, setname] = useState("");
@@ -11,30 +11,30 @@ const Contact = () => {
   const [msg, setMsg] = useState("");
 
   //handle submit button
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      if (!name || !email || !msg) {
-        toast.error("PLease Provide all fields");
-      }
-      const res = await axios.post("/api/v1/portfolio/sendEmail", {
-        name,
-        email,
-        msg,
-      });
-      //validation success
-      if (res.data.success) {
-        toast.success(res.data.message);
-        setname("");
-        setEmail("");
-        setMsg("");
-      } else {
-        toast.error(res.data.message);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   try {
+  //     if (!name || !email || !msg) {
+  //       toast.error("PLease Provide all fields");
+  //     }
+  //     const res = await axios.post("/api/v1/portfolio/sendEmail", {
+  //       name,
+  //       email,
+  //       msg,
+  //     });
+  //     //validation success
+  //     if (res.data.success) {
+  //       toast.success(res.data.message);
+  //       setname("");
+  //       setEmail("");
+  //       setMsg("");
+  //     } else {
+  //       toast.error(res.data.message);
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
   return (
     <>
@@ -44,18 +44,18 @@ const Contact = () => {
             <div className="col-md-6 col-lg-6 col-xl-6 col-sm-12">
               <div className="card1">
                 <div className="row border-line">
-                  <LightSpeed>
+                  {/* <LightSpeed> */}
                     <img
                       src="https://img.freepik.com/free-photo/hot-line-contact-us-call-center-search-interface_53876-124009.jpg?w=2000"
                       alt="ocontact"
                       className="image"
                     />
-                  </LightSpeed>
+                  {/* </LightSpeed> */}
                 </div>
               </div>
             </div>
             <div className="col-lg-6 col-md-6">
-              <Rotate>
+              {/* <Rotate> */}
                 <div className="card2 d-flex card border-0 px-4 py-5">
                   <div className="row">
                     <div className="row">
@@ -103,13 +103,13 @@ const Contact = () => {
                       />
                     </div>
                     <div className="row px-3">
-                      <button className="button" onClick={handleSubmit}>
+                      <button className="button">
                         SEND MESSAGE
                       </button>
                     </div>
                   </div>
                 </div>
-              </Rotate>
+              {/* </Rotate> */}
             </div>
           </div>
         </div>
